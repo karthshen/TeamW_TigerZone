29869702fa4196a157abb80aab662abbdec68961
Michael Arena <mxiia@mxiia.com>
2016-11-05 21:17:55 -0400

    very rudimentary player class

---
+++ Player.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Player {
+
+private:
+
+	Tile tileStack[36]; //initialization of this should be done by the game initialization
+    Tile currentTile; //tile object or class
+
+	void placeTile(tile, board) {
+		//board.location = tile;
+	}
+
+public:
+	bool isHuman;
+	int	score;
+
+};

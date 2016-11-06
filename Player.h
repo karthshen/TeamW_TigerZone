cb54127c987f5ee028c8810c1ea76d3f40ca8f5e
Michael Arena <mxiia@mxiia.com>
2016-11-06 13:57:20 -0500

    added meeple placement prototype

--- Player.h
+++ Player.h
@@ -6,9 +6,14 @@ private:
 
 	Tile tileStack[36]; //initialization of this should be done by the game initialization
     Tile currentTile; //tile object or class
+	Meeple meepls//[??]
 
 	void placeTile(tile, board) {
 		//board.location = tile;
+	}
+	
+	void placeMeeple(meeple, board) {
+		//board.location = meeple;
 	}
 
 public:

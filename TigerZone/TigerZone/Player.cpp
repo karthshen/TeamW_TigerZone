52f243f340a65453ad99a5be42bb8b1dcb85e295
mxiia <mxiia@mxiia.com>
2016-11-11 17:26:12 -0500

    Player file additions

--- TigerZone/TigerZone/Player.cpp
+++ TigerZone/TigerZone/Player.cpp
@@ -2,12 +2,22 @@
 
 using namespace std;
 
-Player::Player();
-Player::~Player();
-// places current tile at location specified by x,y with r rotations
+Player::Player(){
+	isHuman = 1; //this must be changed in the future
+	
+}
+Player::~Player() {
+	 
+	
+}
+
+// places current tile at location specified by x,y
 void placeTile(const Tile& tile, int x, int y) {
+		//calls board class's PlaceTile function
 		Board::PlaceTile(tile, x, y);
 }
 	
-void placeMeeple(const Meeple& meeple, int x, int y);
+void placeMeeple(const Meeple& meeple, int x, int y) {
+	//needs meeple class working first
+}
 

b7c0ac9f31440b535da2c123a8abe9ae6fbffa41
mxiia <mxiia@mxiia.com>
2016-11-11 16:41:40 -0500

    placeTile via board class

--- TigerZone/TigerZone/Player.h
+++ TigerZone/TigerZone/Player.h
@@ -6,10 +6,12 @@ private:
 
 	Tile tileStack[36]; //initialization of this should be done by the game initialization
     Tile currentTile; //tile object or class
-	Meeple meepls//[??]
+	Meeple meeples//[??]
 
-	void placeTile(tile, board) {
-		//board.location = tile;
+	
+	// places current tile at location specified by x,y with r rotations
+	void placeTile(int x int y int r) {
+		Board::PlaceTile(x, y, r)
 	}
 	
 	void placeMeeple(meeple, board) {

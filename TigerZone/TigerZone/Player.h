b1d01d9bcc99d0db4d29a0936f723e25ec17fd8b
mxiia <mxiia@mxiia.com>
2016-11-11 17:21:29 -0500

    Player file

--- TigerZone/TigerZone/Player.h
+++ TigerZone/TigerZone/Player.h
@@ -5,21 +5,22 @@ class Player {
 
 private:
 
+	
+
 	Tile tileStack[36]; //initialization of this should be done by the game initialization
     Tile currentTile; //tile object or class
 	Meeple meeples//[??]
 
 	
 	// places current tile at location specified by x,y with r rotations
-	void placeTile(const Tile& tile, int x, int y) {
-		Board::PlaceTile(tile, x, y)
+	void placeTile(const Tile& tile, int x, int y);
 	}
 	
-	void placeMeeple(meeple, board) {
-		//board.location = meeple;
-	}
+	void placeMeeple(const Meeple& meeple, int x, int y);
 
 public:
+	Player();
+	~Player();
 	bool isHuman;
 	int	score;	
 
